ed991213bb365fca784e897975da51fa07ac67b9
Devon LaFrance <lafrance.devon@gmail.com>
2022-02-07 18:20:00 -0700

    worked on whale background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { Outlet, NavLink } from "react-router-dom";
 
 import logo from './Assets/Images/Logo/Whale_NAWDEV_Right.png'
+import Whale from './Assets/Images/Logo/NAW_Productions_10PercentOpacity.png'
 
 import './Styles.css';
 
 export default function App() {
+  const RandomShift = () => {
+    let number = Math.floor(Math.random() * 1000)
+
+    return (number)
+  }
+
+  const WrapperStyle = {
+    backgroundImage: `url(${Whale})`,
+    backgroundSize: '400vw',
+    backgroundPosition: `${RandomShift()}px`,
+  
+  }
+
   return (
-    <div className="Wrapper">
+    <div className="Wrapper" style={WrapperStyle}>
+
       <div className="Header">
         <nav
           className="Nav"
